dcd075bd37a189954fc42f5516db501c740b8ea3
SonnyFishback <sonny.fishback7@gmail.com>
2022-01-21 12:20:50 -0500

    Initial Commit

---
+++ worksearch/worksearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  worksearchApp.swift
+//  worksearch
+//
+//  Created by Sonny Fishback on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct worksearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
